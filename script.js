f6ce6736e0655e3685c24a22a3142efd5ac28871
Davi Coelho <davicoelho.dev@gmail.com>
2023-03-08 14:33:08 -0300

    HTML structure done

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello world!');
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if(y == 0) {
+        return 'ERROR: cannot divide by zero';
+    } else {
+        return x / y;
+    }
+}
+
+function operator(x, y, op) {
+    switch(op) {
+        case '+':
+            add(x, y);
+            break;
+        case '-':
+            subtract(x, y);
+            break;
+        case '*':
+            multiply(x, y);
+            break;
+        case '/':
+            divide(x, y);
+            break;
+        default:
+            console.log('Operation not available');
+    }
+}
